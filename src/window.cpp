3588851b763749eeb43630ebb950aacdaffaa2e5
eBrnd <github@ebrnd.de>
2013-04-11 21:08:10 +0200

    some more window content

--- src/window.cpp
+++ src/window.cpp
@@ -30,14 +30,52 @@ void Window::init() {
 
 	GtkWidget* binary_status_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 	gtk_box_pack_start(GTK_BOX(binary_status_box), binary_lb, TRUE, TRUE, 0);
-	gtk_box_pack_end(GTK_BOX(binary_status_box), twok_rb, TRUE, TRUE, 0);
-	gtk_box_pack_end(GTK_BOX(binary_status_box), float_rb, TRUE, TRUE, 0);
-	gtk_box_pack_end(GTK_BOX(binary_status_box), dual_rb, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(binary_status_box), dual_rb, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(binary_status_box), twok_rb, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(binary_status_box), float_rb, TRUE, TRUE, 0);
 
 	gtk_box_pack_start(GTK_BOX(main_box), binary_status_box, FALSE, TRUE, 0);
 
+	// binary display unit
+	GtkWidget* binary_display_lb = gtk_label_new("1010 1010 1010 1010 1010 1010 1010 1010");
+	gtk_box_pack_start(GTK_BOX(main_box), binary_display_lb, FALSE, TRUE, 0);
+
+	GtkWidget* hexadecimal_display_lb = gtk_label_new("0xAFFEAFFE");
+	GtkWidget* octal_display_lb = gtk_label_new("0o24 242 424 242");
+
+	GtkWidget* hex_oct_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	gtk_box_pack_start(GTK_BOX(hex_oct_box), hexadecimal_display_lb, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(hex_oct_box), octal_display_lb, TRUE, TRUE, 0);
+	
+	gtk_box_pack_start(GTK_BOX(main_box), hex_oct_box, FALSE, TRUE, 0);
+
+  GtkWidget* bin_display_sep = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
+	gtk_box_pack_start(GTK_BOX(main_box), bin_display_sep, FALSE, TRUE, 0);
+
 	gtk_container_add(GTK_CONTAINER(window), main_box);
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+
+	// main display
+	GtkWidget* main_display_lb = gtk_label_new("1234567890");
+
+	gtk_box_pack_start(GTK_BOX(main_box), main_display_lb, FALSE, TRUE, 0);
+
+
+	// input mode selector
+	GtkWidget* input_mode_lb = gtk_label_new("Input mode:");
+
+	GtkWidget* dec_in_rb = gtk_radio_button_new_with_label(NULL, "Decimal");
+	GtkWidget* hex_in_rb = gtk_radio_button_new_with_label(gtk_radio_button_get_group(GTK_RADIO_BUTTON(dec_in_rb)), "Hex");
+	GtkWidget* dual_in_rb = gtk_radio_button_new_with_label(gtk_radio_button_get_group(GTK_RADIO_BUTTON(dec_in_rb)), "Dual");
+
+	GtkWidget* input_mode_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	gtk_box_pack_start(GTK_BOX(input_mode_box), input_mode_lb, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(input_mode_box), dec_in_rb, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(input_mode_box), hex_in_rb, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(input_mode_box), dual_in_rb, TRUE, TRUE, 0);
+
+	gtk_box_pack_start(GTK_BOX(main_box), input_mode_box, FALSE, TRUE, 0);
 
 	gtk_widget_show_all(window);
 }
